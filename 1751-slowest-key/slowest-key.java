bb24cb9545b310810ba8e591f41035d7bce52bae
PurvaTugaonkar <46226833+PurvaTugaonkar2000@users.noreply.github.com>
2025-01-03 23:42:27 -0800

    Time: 1 ms (99.20%) | Memory: 44.6 MB (31.30%) - LeetSync

--- 1751-slowest-key/slowest-key.java
+++ 1751-slowest-key/slowest-key.java
@@ -1,24 +1,14 @@
 class Solution {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put(keysPressed.charAt(0), releaseTimes[0]);
+        int maxDuration = releaseTimes[0];
+        char slowestkey = keysPressed.charAt(0);
         for(int i = 1; i < releaseTimes.length; i++){
-            int currtime = releaseTimes[i] - releaseTimes[i-1];
-            char currentkey = keysPressed.charAt(i);
-            map.put(currentkey, Math.max(map.getOrDefault(currentkey, 0), currtime));
-        }
-        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
-        pq.addAll(map.entrySet());
-        Character maxKey=null;
-        int maxValue = Integer.MIN_VALUE;
-        for(Map.Entry<Character, Integer> entry : map.entrySet()){
-            char key = entry.getKey();
-            int value = entry.getValue();
-            if (value > maxValue || (value == maxValue && key > maxKey)) {
-                maxValue = value;
-                maxKey = key;
+            int duration = releaseTimes[i] - releaseTimes[i - 1];
+            if(duration > maxDuration || (duration == maxDuration && keysPressed.charAt(i)> slowestkey)){
+                maxDuration = duration;
+                slowestkey = keysPressed.charAt(i);
             }
         }
-        return maxKey;
+        return slowestkey;
     }
 }
