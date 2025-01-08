6233eee4681f885e9b0935ca6439e9e351771645
PurvaTugaonkar <46226833+PurvaTugaonkar2000@users.noreply.github.com>
2025-01-07 20:00:33 -0800

    Time: 2 ms (100.00%) | Memory: 42.5 MB (44.44%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
@@ -11,10 +11,6 @@ class Solution {
         return count;
     }
     private boolean isPrefixAndSuffix(String str1, String str2){
-        int n1 = str1.length(), n2 = str2.length();
-        if(n1 > n2){
-            return false;
-        }
-        return str2.substring(0, n1).equals(str1) && str2.substring(n2 - n1).equals(str1);
+        return str2.startsWith(str1) && str2.endsWith(str1);
     }
 }
