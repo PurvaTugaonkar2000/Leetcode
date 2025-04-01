20b6928ea63ddfdd1eb5a89ef25b1e70f3f226dd
PurvaTugaonkar <46226833+PurvaTugaonkar2000@users.noreply.github.com>
2025-04-01 10:58:40 -0700

    Time: 170 ms (8.31%) | Memory: 119.3 MB (69.42%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,37 +1,26 @@
-class Data {
-    String val;
-    int time;
-    Data(String val, int time) {
-        this.val = val;
-        this.time = time;
-    }
-}
 class TimeMap {
-
-    /** Initialize your data structure here. */
-    Map<String, List<Data>> map;
+    private HashMap<String, TreeMap<Integer, String>> map;
     public TimeMap() {
-        map = new HashMap<String, List<Data>>();
+        map = new HashMap<>();
     }
-
+    
     public void set(String key, String value, int timestamp) {
-        map.computeIfAbsent(key, k -> new ArrayList<Data>()).add(new Data(value, timestamp));
+        map.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
     }
-
+    
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return "";
-        return binarySearch(map.get(key), timestamp);
-    }
-
-    private String binarySearch(List<Data> list, int time) {
-        int left = 0, right = list.size() - 1;
-        while (left < right) {
-            int mid = (left + right + 1) >>> 1;
-            if (time < list.get(mid).time)
-                right = mid - 1;
-            else
-                left = mid;
+        if(!map.containsKey(key)){
+            return "";
         }
-        return list.get(left).time <= time ? list.get(left).val : "";  // left == right
+        TreeMap<Integer, String> timeValueMap = map.get(key);
+        Integer time = timeValueMap.floorKey(timestamp);
+        return (time == null) ? "" : timeValueMap.get(time);
     }
 }
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
