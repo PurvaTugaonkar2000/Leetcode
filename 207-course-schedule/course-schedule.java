8f01f017cdcb501ecf8a0d583bf2dfa0f4d5795d
PurvaTugaonkar <46226833+PurvaTugaonkar2000@users.noreply.github.com>
2025-04-03 00:48:46 -0700

    Time: 7 ms (51.77%) | Memory: 46.3 MB (7.51%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,23 +1,28 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> preMap = new HashMap<>();
-        Set<Integer> visiting = new HashSet<>();
-        for(int i = 0; i < numCourses; i++) preMap.put(i, new ArrayList<>());
-        for(int[] prereq: prerequisites) preMap.get(prereq[0]).add(prereq[1]);
+        Set<Integer> visited = new HashSet<>();
+        for(int i = 0; i < numCourses; i++){
+            preMap.put(i, new ArrayList<>());
+        }
+        for(int[] prereq: prerequisites){
+            preMap.get(prereq[0]).add(prereq[1]);
+        }
         for(int c = 0; c < numCourses; c++){
-            if(!dfs(c, preMap, visiting)) return false;
+            if(!dfs(c, preMap, visited)) return false;
         }
         return true;
+
     }
-    public boolean dfs(int course, Map<Integer, List<Integer>> preMap, Set<Integer> visiting){
-        if(visiting.contains(course)) return false;
+    public boolean dfs(int course, Map<Integer, List<Integer> >preMap, Set<Integer> visited){
+        if(visited.contains(course)) return false;
         if(preMap.get(course).isEmpty()) return true;
-        visiting.add(course);
+        visited.add(course);
         for(int pre: preMap.get(course)){
-            if(!dfs(pre, preMap, visiting)) return false;
+            if(! dfs(pre, preMap, visited)) return false;
         }
-        visiting.remove(course);
+        visited.remove(course);
         preMap.put(course, new ArrayList<>());
         return true;
     }
-} 
+}
