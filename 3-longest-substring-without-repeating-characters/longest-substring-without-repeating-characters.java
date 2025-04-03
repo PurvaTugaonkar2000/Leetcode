640faf637e9b69becb551fd3154cc601678c2575
PurvaTugaonkar <46226833+PurvaTugaonkar2000@users.noreply.github.com>
2025-04-03 14:16:58 -0700

    Time: 7 ms (37.50%) | Memory: 44.9 MB (42.72%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,18 +3,17 @@ class Solution {
         int left = 0;
         int right = 0;
         int max = 0;
-        HashSet<Character> set = new HashSet<>();
+        Set<Character> set = new HashSet<>();
         while(right < s.length()){
             if(!set.contains(s.charAt(right))){
                 set.add(s.charAt(right));
-                right++;
-                max = Math.max(set.size(), max);
-            }
-            else {
+                right ++;
+                max = Math.max(max, set.size());
+            } else {
                 set.remove(s.charAt(left));
-                left++;
+                left ++;
             }
-        } 
-        return max; 
+        }
+        return max;
     }
 }
