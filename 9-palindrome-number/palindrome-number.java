792d5932be73b72b11d374e165492bbd216177fd
PurvaTugaonkar <46226833+PurvaTugaonkar2000@users.noreply.github.com>
2025-11-26 21:34:56 -0800

    Time: 5 ms (69.86%) | Memory: 46 MB (10.17%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0) return false;
-        int temp = x;
+        if(x<0) return false;
+        int original = x;
         int reverse = 0;
-        while(x!=0){
+        while(x > 0){
             int digit = x%10;
-            x = x/10;
-            reverse = reverse* 10 + digit;
+            reverse = reverse*10+digit;
+            x/=10;
         }
-        if(temp == reverse) return true;
-        return false;
+        return original == reverse;
     }
 }
