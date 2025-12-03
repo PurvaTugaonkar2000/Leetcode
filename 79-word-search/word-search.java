78f7996977f91f6eaf8bca198623dc11e632fd09
PurvaTugaonkar <46226833+PurvaTugaonkar2000@users.noreply.github.com>
2025-12-02 23:38:01 -0800

    Time: 135 ms (66.98%) | Memory: 42.7 MB (56.98%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,27 +1,31 @@
 class Solution {
-        int row;
-        int col;
-        Set<Pair<Integer, Integer>> path = new HashSet<>();
     public boolean exist(char[][] board, String word) {
-        row = board.length;
-        col = board[0].length;
-        for(int r = 0; r < row; r++){
-            for(int c = 0; c < col; c++){
-                if(dfs(board, word, r,c,0)) return true;
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                if(dfs(r,c, 0, word, board)) return true;
             }
         }
         return false;
     }
-    public boolean dfs(char[][] board, String word, int r, int c, int i){
-        if(i == word.length()) return true;
-        if(r < 0 || c < 0 || r >= row || c >= col || board[r][c] != word.charAt(i) || path.contains(new Pair<>(r,c))) return false;
-        path.add(new Pair<>(r,c));
-        boolean res = dfs(board, word, r+1, c, i+1) ||
-                      dfs(board, word, r-1, c, i+1) ||
-                      dfs(board, word, r, c+1, i+1) ||
-                      dfs(board, word, r, c-1, i+1);
-        
-        path.remove(new Pair<>(r,c));
-        return res;
+    private boolean dfs(int r, int c, int idx, String word, char[][] board){
+        int rows = board.length;
+        int cols = board[0].length;
+        if(idx == word.length()) return true;
+
+        if(r < 0 || r >= rows || c < 0 || c >= cols) return false;
+        if(board[r][c] != word.charAt(idx)) return false;
+
+        char temp = board[r][c];
+        board[r][c] = '#';
+
+        boolean found = dfs(r - 1, c, idx + 1, word, board)
+        || dfs(r + 1, c, idx + 1, word, board) || dfs(r, c-1, idx+1, word, board) || dfs(r, c+1, idx + 1, word, board);
+
+        board[r][c] = temp;
+
+        return found;
     }
 }
